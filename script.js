3cdffb13a0402d496005b782bcb9732a511cff3b
isabradac <isadorabradaccanovas@gmail.com>
2026-05-29 17:57:24 -0300

    JavaScript do OrbitEye

--- script.js
+++ script.js
@@ -2,46 +2,38 @@ const from = document.getElementById("formulario");
 from.addEventListener("submit", function(event)
 {
     event.preventDefault();
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
+    const nome = document.getElementById("nome").ariaValueMax;
+    const email = document.getElementById("email").ariaValueMax;
 
-    if(nome === ""|| email === "")
+    if(nome ===""|| email === "")
     {
         alert("Preencha todos os campos: ")
     }
     else
     {
-        alert("Mensagem foi enviada com sucesso!!")
+        alert("Mensagem foi enviada com sucesso!")
     }
 });
-function resposta(botao, correto){
-
-    const resultado = document.getElementById("resultadoQuiz");
-
-    const botoes = document.querySelectorAll(".quiz-buttons button");
-
-    botoes.forEach((btn) => {
-
-        btn.disabled = true;
-
-    });
-
-    if(correto){
-
-        botao.style.background = "#22c55e";
-
-        resultado.innerHTML = "Resposta correta!";
-
-        resultado.style.color = "#22c55e";
-
-    } else {
-
-        botao.style.background = "#ef4444";
-
-        resultado.innerHTML = "Resposta incorreta!";
-
-        resultado.style.color = "#ef4444";
+function resposta(botao, correto)
+    {
+        const resultado = document.getElementById("resultadoQuiz");
+        const botoes = document.querySelectorAll(".quiz-buttons button");
+            botoes.forEach((btn) => 
+            {
+                btn.disabled = true;
+            });
+        if(correto)
+        {
+            botao.style.backgroud = "#22c55e";
+            resultado.innerHTML = "Resposta correta";
+            resultado.style.color = "#22c55e";
+        } 
+        else 
+        {
+            botao.style.backgroud = "#ef4444";
+            resultado.innerHTML = "Resposta Incorreta"
+            resultado.style.color = "#ef4444";
+        }
 
     }
 
-}
